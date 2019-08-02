45afde79ec1e7531a34c159c574bdbc49fb37807
Rjones <rjonessoft@gmail.com>
2019-08-02 18:47:20 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Text_Adventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to The Forest");
+            Console.WriteLine("Are you ready for an adventure? (yes/no)");
+
+            string answer = Console.ReadLine();
+
+            if(answer.ToLower() == "yes")
+            {
+                Console.WriteLine("Sweet! Let's go!");
+            }
+            else if (answer == "no")
+            {
+                Console.WriteLine("Well too bad. Better get ready anyway");
+            }
+        }   
+                
+
+    }
+}
