5060b5d668b915cccf1c357d8b622bdb233a2a0f
Rjones <rjonessoft@gmail.com>
2019-08-02 19:22:21 -0500

    Brandon made me make it longer

--- Program.cs
+++ Program.cs
@@ -19,8 +19,39 @@ namespace Text_Adventure
             {
                 Console.WriteLine("Well too bad. Better get ready anyway");
             }
-        }   
-                
 
+            Console.WriteLine("You must choose to go left or right");
+            answer = Console.ReadLine();
+
+            if (answer.ToLower() == "left")
+            {
+                Console.WriteLine("You immediatley get punched by a ghost");
+                Console.WriteLine("You get up and must decide if you are going to swing on the vines or continue walking. (vines/continue)");
+                answer = Console.ReadLine();
+                if(answer.ToLower() == "vines")
+                {
+                    Console.WriteLine("You turn into a monkey");
+                }
+                else if (answer == "continue")
+                {
+                    Console.WriteLine("You get thirsty and find a pond");
+                }
+            }
+            else if (answer.ToLower() == "right")
+            {
+                Console.WriteLine("You immediatley meet a green monster");
+                Console.WriteLine("You must decide if you are going to run or attack. (run/attack)");
+                answer = Console.ReadLine();
+                if(answer.ToLower() == "run")
+                {
+                    Console.WriteLine("You escape the green monster and keep running");
+                }
+                else if ( answer == "attack")
+                {
+                    Console.WriteLine("The green monster begins to cry because he wanted to be friends");
+                }
+            }
+
+        }   
     }
 }
